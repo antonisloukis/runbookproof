6a910c8b2fd70c3a8a89cbc7d1397736000c2399
Antonis Loukis <antonhs.loukhs@gmail.com>
2026-08-04 03:02:51 +0300

    Add kubectl verification pack

--- src/runbookproof/packs/__init__.py
+++ src/runbookproof/packs/__init__.py
@@ -3,6 +3,7 @@
 from runbookproof.packs.bash import BashPack
 from runbookproof.packs.docker import DockerPack
 from runbookproof.packs.git import GitPack
+from runbookproof.packs.kubectl import KubectlPack
 from runbookproof.packs.node_package import NodePackagePack
 from runbookproof.packs.python_package import PythonPackagePack
 from runbookproof.packs.universal import UniversalPack
@@ -11,6 +12,7 @@ __all__ = [
     "BashPack",
     "DockerPack",
     "GitPack",
+    "KubectlPack",
     "NodePackagePack",
     "PythonPackagePack",
     "UniversalPack",
